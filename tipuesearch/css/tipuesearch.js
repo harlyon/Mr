8218f3a6700b8a37332e832490268b32231174c9
Harrison Ekpobimi <okeharlyon@gmail.com>
2017-10-20 11:03:56 +0200

    Add files via upload

---
+++ tipuesearch/css/tipuesearch.js
@@ -0,0 +1,109 @@
+var tipuesearch = {"pages": [
+{
+	"title": "eGroup",
+	"text": `
+	eGroup versteht sich als globale, ganzheitliche Dienstleisterin für Löhne. Neben unserem Kerngeschäft, der digitalen Administration von Löhnen, bieten wir unseren Kunden umfassende Dienstleistungen in direktem Zusammenhang dazu an. Dazu zählen insbesondere die direkte Abrechnung mit Sozialversicherungsträgern, das Vermitteln und Verwalten von Versicherungsanschlüssen, Case Management Dienstleistungen sowie das Aufsetzen, Führen und Abrechnen der betrieblichen Altersversorgung, die Rechts- und Steuerberatung sowie die Buchführung.  Die in den Tochtergesellschaften Realisator AG, eCare AG und eGroup International AG beschäftigten Mitarbeiter/Innen administrieren europaweit monatlich rund 80‘000 Löhne was einer jährlichen Lohnsumme von rund EUR 1 Mrd. entspricht.
+	`,
+	"tags": "partner, eGroup, realisator",
+	"url": "/e_eGroup.html"
+},
+{
+	"title": "Pensionkasse pro",
+	"text": `
+	Die pensionkasse pro positioniert sich als flexible, innovative, effiziente und kostengünstige Sammelstiftung im Markt. Seit der Gründung 2002 haben sich ihr mehr als 7‘900 Unternehmen mit rund 61‘000 Arbeitnehmerinnen und Arbeitnehmern für die berufliche Vorsorge angeschlossen. Damit zählt die pensionskasse pro zu den grössten unabhängigen Sammelstiftungen der Schweiz. Die pensionkasse pro bietet Vorsorgeleistungen, die über das gesetzliche Minimum hinausgehen, abgestimmt nach individuellen Bedürfnissen und zu fairen Konditionen.
+	`,
+	"tags": "partners, Pensionkasse pro",
+	"url": "/e_pensionkasse-pro.html"
+	},
+{
+"title": "SWICA",
+"text": `
+SWICA ist eine der führenden Kranken- und Unfallversicherungen der Schweiz mit rund 1,3 Millionen Versicherten und 27‘000 Unternehmenskunden. SWICA ist die einzige Gesundheitsorganisation mit einer integralen Dienstleistungskette bei allen Fragen zur Gesundheit – von der medizinischen Versorgung über die kompetente Begleitung erkrankter oder verunfallter Menschen bis hin zur Gesundheitsförderung in Betrieben.
+`, 
+"tags": "parners, SWICA",
+"url": "/e_swica.html"
+},
+{
+"title": "Profil",
+"text":`
+Rechtsdienst für den Personalverleih. Branchenlösung Krankentaggeld für den Personalverleih. Case Management und Gesundheitsförderung am Arbeitsplatz. Arbeitsrecht und Steuerberatung.
+`,
+"tags": "Über uns, Profil",
+"url": "/e_profil.html"
+},
+{
+"title": "Organisation",
+"text": `Organisation`,
+"tags": "Über uns, Organisation",
+"url": "e_organisation.html"
+},
+{
+"title": "Standort",
+"text":`
+Unsere modernen und hellen Büros befinden sich inmitten der aufstrebenden Wirtschaftsregion Zug West. Sie können uns wie folgt erreichen: Mit der Bahn: S5 von Zürich oder Zug, Haltestelle Steinhausen Rigiblick Mit dem Auto: A4 von Zürich oder Luzern bis Verzweigung Blegi, A4a, Ausfahrt 2 Cham/Zug/Steinhausen, Richtung Zug/Chamerstrasse, nach ca. 600m links abbiegen auf Alte Steinhauserstrasse, dann nach rechts Hinterbergstrasse. Markierte Besucherparkplätze finden sich rund um das Gebäude.
+`,
+"tags": "Über uns, Standort",
+"url": "/e_standort.html"
+},
+{
+"title": "Wir als Arbeitgeber",
+"text":`
+eCare AG ist ein junges, aufstrebendes Spin Off der Realisator AG, welche sich in über 35 Jahren eine Marktführerschaft im Bereich der Administration und Beratung von Personalverleihern aufgebaut hat. Wir kombinieren Erfahrung und Know-how mit Kreativität und dem Mut, Neues zu wagen und Grenzen auszuloten. eCare bietet einen modernen Arbeitsplatz, attraktive Anstellungsbedingungen und ein Arbeitsklima und Umfeld, welches auf gegenseitiger Wertschätzung, Teamgeist und Vertrauen basiert. Unsere Mitarbeiterinnen und Mitarbeiter sind unser höchstes Gut. Ihr Leistungswille und ihre unternehmerische Denkweise verkörpern die Werte von eCare. Wir bekennen uns zu Chancengleichheit und der Förderung der physischen und psychischen Gesundheit unserer Mitarbeiterinnen und Mitarbeiter.
+`,
+"tags": "Über uns, Karriere, Wir als Arbeitgeber, ecare",
+"url": "/e_wir_als_srbeitgeber.html"
+},
+/*{
+"title":"Offen Stellen",
+"text":``, "tags": "Offen Stellen", "": ""
+},*/
+{
+"title": "Externer Rechtsdienst Personalverleih",
+"text":`
+Im Auftrag der und exklusiv für Kunden der Realisator AG bieten wir folgende Leistungen: Arbeitsrecht Telefonische und schriftliche Rechtsauskünfte Beratung bei richtigen Anwendung von Gesamtarbeitsverträgen  Vertragsausgestaltung und aussergerichtliche Rechtsvertretung Lohnbuchkontrollen Beratung und Vertretung im Rahmen von GAV- Lohnbuchkontrollen Prüfung von Kontrollberichten und Entscheiden Verfassen von Stellungnahmen und Rekursschriften Gründungen und Betriebsbewilligung  Firmengründungen im Personalverleih Beratung und Vertretung im Rahmen von Betriebsbewilligungen nach AVG Redaktion von AVG-Dokumenten. Handels – und Vertragsrecht Rechtsberatung Firmenumstrukturierungen, Unternehmensnachfolge Ausgestaltung von Verträgen, Statuten und Reglementen.
+`, 
+"tags": "Dienstleistungen, Externer Rechtsdienst Personalverleih",
+"url": "/e_externer.html"
+},
+{
+"title": "Krankentaggeldversicherung für Personalverleiher",
+"text":`
+Als Agentur unseres Versicherungspartners SWICA und exklusiv für Kunden der Realisator AG bieten wir folgende Leistungen: Vermittlung von Anschlussverträgen GAV- konforme Krankentaggeldversicherung (Branchenlösung) Profitieren Sie von tiefen Versicherungsprämien dank Branchenspezialisierung Jährliche Prämienanpassung aufgrund effektivem Schadenverlauf möglich Schadenabwicklung / Case Management  Rasche und effiziente Schadenabwicklung durch Spezialisten der eCare und SWICA mit Know-how und Erfahrung im Personalverleih Integriertes Case Management und professionelle Fallbegleitung durch SWICA Administration Einfache Fallmeldung über Onlineportal Rasche Abrechnung und Zahlung der Taggelder mittels der führenden Branchensoftware E@syTemp Informationen über abgerechnete Taggelder jederzeit in E@syTemp verfügbar Rechtsdienst Rechtliche Unterstützung und Beratung durch integrierten Rechtsdienst der Realisator AG  Spezifisches Know-how und Erfahrung im Personalverleih
+`, 
+"tags": "Dienstleistungen, Krankentaggeldversicherung für Personalverleiher",
+"url": "/e_krankentaggeldversicherung.html"
+},
+{
+"title": "Case Management",
+"text":`
+In Zusammenarbeit mit unserem Partner SWICA und exklusiv für Kunden der pensionskasse pro bieten wir folgende Leistungen: Absenzenmanagement Vertiefte Analyse aller gesundheitsbedingten Absenzen anhand von klar definierten Triagekriterien und mittels eigens entwickelter Softwareapplikation Ziel: Vermeiden von Langzeitabsenzen und Senkung der Schadenquote Fokussierung auf psychische Leiden und Rückenleiden Case Management Professionelle Fallbegleitung durch SWICA und eCare Langzeitabsenzfälle mit Lenkungspotential werden durch qualifizierte Case Manager persönlich betreut, immer mit dem Ziel, sie in den Arbeitsmarkt zu reintegrieren Hohe Erfolgsquote.
+`, 
+"tags": "Dienstleistungen, Case Management, swica, Pensionkasse pro",
+"url": "/e_case-management.html"
+},
+{
+"title": "Rechtsberatung",
+"text":`
+Auf Wunsch und im Mandatsverhältnis bieten wir folgende Dienstleistungen: Rechtsberatung Beratung und aussergerichtliche Vertretung im Arbeits -, Handels- und Vertragsrecht Ausgestaltung von Verträgen, Statuten und Reglementen Firmenumstrukturierungen, Unternehmensnachfolge Netzwerk von assoziierten Anwälten für gerichtliche Vertretungen Lohnbuchkontrollen Beratung und Vertretung im Rahmen von GAV - Lohnbuchkontrollen Prüfung von Kontrollberichten und Entscheiden Verfassen von Stellungnahmen und Rekursschriften
+`, 
+"tags": "Dienstleistungen, Rechtsberatung", 
+"url": "/e_rechtsberatung.html"
+},
+{
+"title": "Steuerberatung",
+"text":`
+Auf Wunsch und im Mandatsverhältnis bieten wir folgende Dienstleistungen: Steuerrecht Steuerberatung und - planung in nationalen und internationalen Steuerfragen für Privatpersonen und Unternehmen im Bereich der Einkommens - und Vermögenssteuer, Gewinn - und Kapitalsteuer, Erbschafts - und Schenkungssteuer, Grundstückgewinn - und Handänderungssteuer, Verrechnungssteuer, Stempelabgaben und Mehrwertsteuer Unterstützung bei Steuerrevisionen sowie Due Dilligence - Prüfungen Steuervertretung für natürliche und juristische Personen Erwirken von rechtsverbindlichen Steuervereinbarungen (Steuerrulings) Steuerberatung und – planung bei Gründungen, Liquidationen, Umstrukturierungen sowie bei Nachfolgeregelung
+`, 
+"tags": "Dienstleistungen, Steuerberatung", 
+"url": "/e_steuerberatung.html"
+},
+{
+"title": "Unsere Koordinaten",
+"text":`
+Legal Services Leiter Legal Services: lic. iur. Roger Kühne, phone: +41 44 744 96 70, mail: rechtsdienst@realisator.ch. SWICA Agency Service phone: +41 44 744 96 60, mail: info@ecare.ch, Case Management, phone: +41 44 744 96 60, mail: cm@ecare.ch. Tax Services Leiterin Tax Services: Tamara Tormen, dipl. Steuerexpertin, phone: +41 44 744 96 63, mail: ttormen@ecare.ch
+`, 
+"tags": "Kontakt, Unsere Koordinaten", 
+"url": "/e_kontakt.html"
+}, 
+    ]
+};
